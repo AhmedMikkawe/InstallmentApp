09fc2ba3ac2daea9ce43994fee8cc6f49eb86b05
Ahmed Mikkawe <ah.mikkawe@gmail.com>
2023-02-16 23:35:26 +0200

    add kafeel relationship to User Model and create Kafeel model and create kafeel table migration

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['name' => 'super-admin']);
+        User::create([
+            "fullname" => "Admin",
+            "username" => "admin",
+            "email" => "admin@gmail.com",
+            "password"=> Hash::make("123456"),
+            "nationalId" => "1234567",
+            "nationalId-photo"=>  "",
+            "phone-number"=> "1234567"
+        ]);
+        $user = User::with('roles')->where('email','admin@gmail.com')->first();
+        $user->assignRole('super-admin');
+    }
+}
