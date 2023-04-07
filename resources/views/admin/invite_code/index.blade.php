2a7b7a316c39b31c70b90a8276c98c5ecb343ac6
Ahmed Mikkawe <ah.mikkawe@gmail.com>
2023-04-07 04:27:11 +0200

    added the ability to send an invitation with whatsapp

--- resources/views/admin/invite_code/index.blade.php
+++ resources/views/admin/invite_code/index.blade.php
@@ -43,6 +43,15 @@
                     <td>
                         <a class="btn btn-warning" href="{{ route("invite_code.edit",$invite_code->id) }}">Edit</a>
                         <button class="btn btn-info copyCode" data-clipboard-text="{{route("auth.register")}}?code={{$invite_code->code}}">Copy</button>
+                        @if($invite_code->valid == 1)
+                            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#whatsapp-modal"
+                                    data-invite-code="{{$invite_code->code}}"
+                            >
+                                أرسل دعوة عن طريق واتساب
+                            </button>
+                        @endif
+
+
                     </td>
                   </tr>
 
@@ -55,11 +64,67 @@
 
         </div>
       </section>
+    <div class="modal fade" id="whatsapp-modal" tabindex="-1" role="dialog" aria-labelledby="share code to whatsapp" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+
+                    <h5 class="modal-title" id="whatsapp-modal-title">إرسال كود الدعوة على واتس اب</h5>
+                </div>
+                <div class="modal-body">
+
+                    <div class="form-group">
+                        <label for="invCode">الكود</label>
+                        <input type="email" class="form-control" id="invCode" disabled>
+                    </div>
+                    <div class="form-group">
+                        <label for="phone">رقم الهاتف</label>
+                        <input type="tel" class="form-control" id="phone" >
+                        <small class="form-text text-muted">
+                            اكتب رقم الهاتف كامل بمفتاح الدولة ولكن بدون علامة (+)
+                        </small>
+
+                    </div>
+                    <div class="form-group">
+                        <label for="invText">Example textarea</label>
+                        <textarea class="form-control" id="invText" rows="3">
+                            الرجاء استخدام هذا الرابط اثناء التسجيل
+                        </textarea>
+                    </div>
+
+                </div>
+
+                <div class="modal-footer">
+                    <a id="send-invite" href="#" class="btn btn-success btn-block" target="_blank">إرسال</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
 @section('scripts')
     <script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.10/dist/clipboard.min.js"></script>
     <script>
         new ClipboardJS('.copyCode');
+        $('#whatsapp-modal').on('shown.bs.modal', function (e) {
+             let incode = $('#invCode').val(e.relatedTarget.attributes[4].textContent);
+            let phone = $("#phone").val("");
+            let tex = $("#invText").val("الرجاء استخدام هذا الرابط اثناء التسجيل:");
+            $("#invText").val( tex.val() + " " + "{{url('/?code=')}}" + incode.val());
+
+        })
+        $("#send-invite").on('click',function(e){
+           e.preventDefault();
+           const WHATSAPP_LINK = "https://api.whatsapp.com/send";
+           let phone = $("#phone").val();
+           let message = $("#invText").val();
+           let newLink = WHATSAPP_LINK + `?phone=${phone}&text=${message}`;
+            e.target.attributes[1].value = newLink;
+            window.open(newLink,"_blank");
+        });
     </script>
 
 
